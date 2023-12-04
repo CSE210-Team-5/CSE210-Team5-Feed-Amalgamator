576274a3ced929be7caff61aa03abaa91bd20fc9
Wei Jie CHIA <wchia@ucsd.edu>
2023-12-03 21:55:50 -0800

    Fixed bug in tests

--- tests/test_mastodon_oauth_interface.py
+++ tests/test_mastodon_oauth_interface.py
@@ -10,7 +10,7 @@ from feed_amalgamator.helpers.mastodon_oauth_interface import MastodonOAuthInter
 
 class TestOauthInterface(unittest.TestCase):
     def setUp(self) -> None:
-        test_config_loc = "test_config/test_mastodon_client_info.ini"
+        test_config_loc = Path("tests/test_config/test_mastodon_client_info.ini")
         parser = configparser.ConfigParser()
         parser.read(test_config_loc)
         test_log_root = parser["TEST_SETTINGS"]["test_log_root"]
@@ -20,8 +20,7 @@ class TestOauthInterface(unittest.TestCase):
         logger = LoggingHelper.generate_logger(logging.INFO, test_log_file, logger_name)
         self.logger = logger
         self.client = MastodonOAuthInterface(test_config_loc, logger)
-        self.client_domain = parser["APP_TOKENS"]["client_domain"]  # Required to be passed in as a parameter
-        self.user_auth_code = parser["APP_TOKENS"]["user_auth_code"]
+        self.client_domain = parser["APP_TOKENS"]["CLIENT_DOMAIN"]  # Required to be passed in as a parameter
 
     def test_verify_user_provided_domain(self):
         with_https = "https://mastodon.social"
@@ -34,23 +33,18 @@ class TestOauthInterface(unittest.TestCase):
             (True, wanted_result),
         )
 
-        another_valid_domain = "mstdn.jp"
-        self.assertEqual(self.client.verify_user_provided_domain(another_valid_domain), (True, another_valid_domain))
-
         mangled_domain = "mastodo.social"
         self.assertEqual(self.client.verify_user_provided_domain(mangled_domain)[0], False)
 
     def test_generate_user_token(self):
         # No client has been started yet, AssertionError should be thrown
-        self.assertRaises(AssertionError, self.client.generate_user_access_token("undefined"))
+        self.assertRaises(AssertionError, self.client.generate_user_access_token, "undefined")
 
         self.client.start_app_api_client(self.client_domain)  # Sets self.api_client
 
         wrong_auth_code = "Sousou no Frieren"
-        self.assertRaises(
-            InvalidApiInputError,
-            self.client.generate_user_access_token(wrong_auth_code),
-        )
+        self.assertRaises(InvalidApiInputError, self.client.generate_user_access_token,
+                          wrong_auth_code)
 
         # Testing a CORRECT auth code cannot be done automatically as it requires
-        # a manual redirect to a page where a user has to log in. As such, as only test the wrong situation
+        # a manual redirect to a page where a user has to log in. As such, we only test the wrong situation
