033a8706d43b53662330b17f6ec5cc93673a4735
Srinivas Raman <srinivasraman18@gmail.com>
2023-12-12 19:15:38 -0800

    Added support for azure db and fixed exception handling bugs

--- feed_amalgamator/__init__.py
+++ feed_amalgamator/__init__.py
@@ -1,20 +1,45 @@
 import os
+import configparser
+import urllib
+from pathlib import Path
 
 from flask import Flask, redirect, url_for
 
 from . import auth, feed
 from feed_amalgamator.helpers.db_interface import dbi
 from feed_amalgamator.helpers import error_handler # noqa
+from feed_amalgamator.constants.common_constants import CONFIG_LOC
 
 
 def create_app(test_config=None, db_file_name=None):
+    parser = configparser.ConfigParser()
+    # Setting up the loggers and interface layers
+    with open(CONFIG_LOC) as file:
+        parser.read_file(file)
     # create and configure the app
+    environment_type = parser["ENVIRONMENT"]["ENVIRONMENT"]
+    secret_key = parser["ENVIRONMENT"]["SECRET_KEY"]
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev",  # Probably something that should not be hard coded in plaintext
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
-    )
+    app.config["SECRET_KEY"] = secret_key
+    db_location = None
+    if environment_type == "dev":
+        app.config["DATABASE"] = os.path.join(app.instance_path, "flaskr.sqlite")
+        try:
+            os.makedirs(app.instance_path)
+        except OSError:
+            pass
+        # Default db location
+        if db_file_name is None:
+            db_location = "sqlite:///{loc}".format(loc=os.path.join(app.instance_path, "flaskr.sqlite"))
+        else:
+            db_location = "sqlite:///{loc}".format(loc=os.path.join(app.instance_path, db_file_name))
+    elif environment_type == "prod":
+        connection_string = parser["DATABASE"]["CONNECTION_STRING"]
+        uri_prefix = parser["DATABASE"]["URI_PREFIX"]
+        params = urllib.parse.quote_plus(connection_string)
+        db_location = uri_prefix.format(params)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_location
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile("config.py", silent=True)
@@ -22,23 +47,8 @@ def create_app(test_config=None, db_file_name=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
     app.register_blueprint(auth.bp)
     app.register_blueprint(feed.bp)
-
-    if db_file_name is None:
-        # Default db location
-        db_location = "sqlite:///{loc}".format(loc=os.path.join(app.instance_path, "flaskr.sqlite"))
-    else:
-        db_location = "sqlite:///{loc}".format(loc=os.path.join(app.instance_path, db_file_name))
-
-    # Hard coded db location atm, but we will need to refactor this entire init
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_location
     dbi.init_app(app)
 
     @app.route("/", methods=["GET"])
--- feed_amalgamator/helpers/error_handler.py
+++ feed_amalgamator/helpers/error_handler.py
@@ -1,12 +1,13 @@
 import logging
 import configparser
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from pathlib import Path
 
 from feed_amalgamator.constants.common_constants import CONFIG_LOC
 from feed_amalgamator.helpers.custom_exceptions import (
     InvalidCredentialsError, NoContentFoundError, InvalidDomainError,
-    ServiceUnavailableError, IntegrityError)
+    ServiceUnavailableError, IntegrityError, AddServerIntegrityError, AddServerServiceUnavailableError,
+    AddServerInvalidCredentialsError)
 from feed_amalgamator.auth import bp as auth_bp
 from feed_amalgamator.feed import bp as feed_bp
 from feed_amalgamator.helpers.logging_helper import LoggingHelper
@@ -25,6 +26,7 @@ auth_logger = LoggingHelper.generate_logger(logging.INFO, log_file_loc, "auth_pa
 @feed_bp.errorhandler(NoContentFoundError)
 @feed_bp.errorhandler(ServiceUnavailableError)
 @feed_bp.errorhandler(IntegrityError)
+@feed_bp.errorhandler(InvalidCredentialsError)
 def handle_feed_exceptions(err):
     feed_logger.exception(err)
     return render_template(err.args[0]['redirect_path'], error_message=err.args[0]['message'])
@@ -35,3 +37,12 @@ def handle_feed_exceptions(err):
 def handle_auth_exceptions(err):
     auth_logger.exception(err)
     return render_template(err.args[0]['redirect_path'], error_message=err.args[0]['message'])
+
+
+@feed_bp.errorhandler(AddServerInvalidCredentialsError)
+@feed_bp.errorhandler(AddServerServiceUnavailableError)
+@feed_bp.errorhandler(AddServerIntegrityError)
+def handle_exception_and_redirect(err):
+    feed_logger.exception(err)
+    flash(err.args[0]['message'])
+    return redirect(url_for(err.args[0]['redirect_path']))
